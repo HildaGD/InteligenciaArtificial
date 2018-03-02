f0ddd543226f25519d4e43c909a2636cef42b7f9
HildaGD <hilda.guevara20@gmail.com>
2018-03-02 17:31:14 -0600

    calculadora

---
+++ Proyecto 1/calculadora.pl
@@ -0,0 +1,34 @@
+%CALCULADORA%
+
+suma:- write('ingrese primer numero: '),
+    read(A),
+    write('ingrese segundo numero: '),
+    read(B),
+    C is A+B,
+    format('El resultado es: ~w',[C]).
+
+
+resta:-write('Ingrese primer numero: '),
+    read(A),
+    write('ingrese segundo numero: '),
+    read(B),
+    C is A-B,
+    format('El resultado es: ~w',[C]).
+
+
+multiplicacion:-write('Ingrese primer numero: '),
+    read(A),
+    write('ingrese segundo numero: '),
+    read(B),
+    C is A*B,
+    format('El resultado es: ~w',[C]).
+
+
+division:-write('Ingrese primer numero: '),
+    read(A),
+    write('ingrese segundo numero: '),
+    read(B),
+    C is A/B,
+    format('El resultado es: ~w',[C]).
+
+
